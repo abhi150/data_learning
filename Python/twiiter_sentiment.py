f190cdccd6aea90ed4d224056c4da7c944610919
abhilash <abhilash@getsimpl.com>
2019-09-10 20:35:46 +0530

    added tweeter

---
+++ Python/twiiter_sentiment.py
@@ -0,0 +1,30 @@
+import tweepy
+from textblob import textblob
+
+consumer_key = 'iicRdbtAYItuoFazHY90B6kFT'
+consumer_secret = '8pdOVfLYCMuUYKxUFwPUucJxivq1WG4mLxbqlQqqbdJIVcfAGS'
+
+
+access_token = '2234539196-mzMmrv71of4DooW5jkWFQHDBA3fOkpYqqOJbROD'
+access_token_secret = 'YEkESqNwFJn6Kvf1hlxTcHd6nXfaOOl0xuLwLq8J56Poj'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# Step 3 - Retrieve Tweets
+public_tweets = api.search('Trump')
+
+# CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+# and label each one as either 'positive' or 'negative', depending on the sentiment
+# You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+
+    # Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+    print("")
