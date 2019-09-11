e80006e99235691353f99390942c66fe8732baa9
abhilash <abhilash@getsimpl.com>
2019-09-11 23:24:45 +0530

    added recommendation

--- Python/twiiter_sentiment.py
+++ Python/twiiter_sentiment.py
@@ -1,5 +1,6 @@
 import tweepy
-from textblob import textblob
+from textblob import TextBlob
+
 
 consumer_key = 'iicRdbtAYItuoFazHY90B6kFT'
 consumer_secret = '8pdOVfLYCMuUYKxUFwPUucJxivq1WG4mLxbqlQqqbdJIVcfAGS'
@@ -8,13 +9,15 @@ consumer_secret = '8pdOVfLYCMuUYKxUFwPUucJxivq1WG4mLxbqlQqqbdJIVcfAGS'
 access_token = '2234539196-mzMmrv71of4DooW5jkWFQHDBA3fOkpYqqOJbROD'
 access_token_secret = 'YEkESqNwFJn6Kvf1hlxTcHd6nXfaOOl0xuLwLq8J56Poj'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)exi
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
 # Step 3 - Retrieve Tweets
 public_tweets = api.search('Trump')
+
+
 
 # CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
 # and label each one as either 'positive' or 'negative', depending on the sentiment
